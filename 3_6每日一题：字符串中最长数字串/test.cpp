e52b30233e87fa396a496b2bca40c72135b8e7c5
浅忆梦微凉 <3538922590@qq.com>
2019-03-07 00:53:25 +0800

    存在bug

---
+++ 3_6每日一题：字符串中最长数字串/test.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+
+bool IsNum(char ch){
+	auto temp = ch + 0;
+	if (48 <= temp && temp <= 57){
+		return true;
+	}
+	return false;
+}
+
+int main(){
+	vector<string> array;
+	string str;
+	string temp = "";
+	cin >> str;
+	if (str.empty()){
+		cout << "" << endl;
+		system("pause");
+		return 0;
+	}
+	auto it = str.begin();
+	while (it != str.end()){
+		auto nustr = it;
+		while (it != str.end()&&(!IsNum(*it))){
+			it++;
+		}
+		if (it == str.end()){
+			cout << "" << endl;
+			system("pause");
+			return 0;
+		}
+		nustr = it;
+		while (nustr != str.end()&&IsNum(*nustr)){
+			nustr++;
+		}
+		
+		for (auto i = it; i != nustr; ++i){
+			char ch = *i;
+			char tmp[1];
+			tmp[0] = ch;
+			string res(tmp, 1);
+			temp += res;
+		}
+
+		array.push_back(temp);
+		temp = "";
+		it = nustr;
+	}
+	int maxstr = 0;
+	for (int i = 0; i < array.size(); ++i){
+		if (array[i].size() >= maxstr){
+			maxstr = array[i].size();
+		}
+	}
+	for (int i = 0; i < array.size(); ++i){
+		if (maxstr == array[i].size()){
+			cout << array[i] << endl;
+		}
+	}
+	system("pause");
+	return 0;
+}
